232de49c86bba5a6978db25cff3eb4e6a66ad8b6
chalvoker <chalvoker@noreply.example.org>
2021-04-28 19:38:41 +0000

    Update 'myfamily.sh'

--- myfamily.sh
+++ myfamily.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-curl -s  https://01.alem.school/assets/superhero/all.json| jq '.[] | select(.id == '$HERO_ID') |.connections|.relatives'| sed 's/.$//'| sed 's/^.//' | tr -d "\n"
+curl -s  https://01.alem.school/assets/superhero/all.json| jq '.[] | select(.id == '$HERO_ID') |.connections|.relatives'| sed 's/.$//'| sed 's/^.//'
 
