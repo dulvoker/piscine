55f897cb0a814a2eb8a18e32a179a411f4405941
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-04-28 20:21:53 +0600

    Task3

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
-echo 8047;
-
+#! /bin/bash
+curl -s https://01.alem.school/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"chalvoker\"}}){id}}"}' | jq '.data.user[0].id'
