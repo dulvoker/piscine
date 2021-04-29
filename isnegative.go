2879e80facd285a43e23e6104361955231c43f4c
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-04-29 19:06:07 +0600

    isnegative

--- isnegative.go
+++ isnegative.go
@@ -1,11 +1,11 @@
 package piscine
 
-import "fmt"
+import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
 	if nb < 0 {
-		fmt.Println("T")
+		z01.PrintRune('T')
 	} else {
-		Println("F")
+		z01.PrintRune('F')
 	}
 }
