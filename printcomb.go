56f0a3cf72d882d67ff08a0dccc63231edb30181
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-04-29 19:36:02 +0600

    asd

--- printcomb.go
+++ printcomb.go
@@ -4,14 +4,14 @@ import "github.com/01-edu/z01"
 
 func PrintComb() {
 	for r := 48; r < 56; r++ {
-		z01.PrintRune(rune(r))
 		for i := r + 1; i < 58; i++ {
-			z01.PrintRune(rune(i))
-			for z := r + 1; z < 58; z++ {
+			for z := i + 1; z < 58; z++ {
+				z01.PrintRune(rune(r))
+				z01.PrintRune(rune(i))
 				z01.PrintRune(rune(z))
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
 			}
 		}
-		z01.PrintRune(',')
-		z01.PrintRune(' ')
 	}
 }
