5dc94e10dfdce52380fc01e14bffe7833f486ee1
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-01 04:30:19 +0600

    sd

--- ultimatepointone.go
+++ ultimatepointone.go
@@ -1,5 +1,21 @@
-package piscine
+package main
 
-func UltimatePointOne(n ***int) {
-	***n = 1
+func UltimateDivMod(a *int, b *int) {
+	c := a
+	*a = *a / *b
+	*b = c % *b
 }
+
+import (
+	"fmt"
+	piscine "github.com/01-edu/z01"
+)
+
+func main() {
+	a := 13
+	b := 2
+	piscine.UltimateDivMod(&a, &b)
+	fmt.Println(a)
+	fmt.Println(b)
+}
+
