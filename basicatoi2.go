ec596ef79219bb0cf0e5f59dae261305768b96ac
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-01 07:03:22 +0600

    asd

---
+++ basicatoi2.go
@@ -0,0 +1,13 @@
+package piscine
+
+func BasicAtoi2(s string) int {
+	sam := []rune(s)
+	prev := 0
+	for i := 0; i < len(s); i++ {
+		if sam[i] > 47 && sam[i] < 58 {
+			prev = prev * 10
+			prev = int(sam[i]) - 48 + prev
+		}
+	}
+	return prev
+}
