cb7363b6a7070aec947a8f68ac5e9e7b1d615e21
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-01 06:26:03 +0600

    asd

---
+++ basicatoi.go
@@ -0,0 +1,12 @@
+package piscine
+
+func BasicAtoi(s string) int {
+	sam := []rune(s)
+	summa = 0
+	for i := 0; i < len(s); i++ {
+		if sam[i] > 48 && sam[i] < 58 {
+
+		}
+	}
+	return string(reversenoyet)
+}
