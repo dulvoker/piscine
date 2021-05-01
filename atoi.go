3627518114b567c4aa0045e4bf11d905c293c167
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-01 07:30:34 +0600

    asd

--- atoi.go
+++ atoi.go
@@ -1,29 +1,33 @@
 package piscine
 
-func atoi(s string) int {
+func Atoi(s string) int {
 	sam := []rune(s)
 	prev := 0
 	check := 0
-	checkminus := 0
+	checkplus := 0
 	for i := 0; i < len(s); i++ {
 		if sam[i] > 47 && sam[i] < 58 || sam[i] == 43 || sam[i] == 45 {
 			if sam[i] == 45 || sam[i] == 43 {
 				if sam[i] == 43 {
-					checkminus = 1
+					checkplus = 1
 				}
 				check = 1 + check
 				if check > 1 {
 					return 0
 				}
 			}
-			prev = prev * 10
-			prev = int(sam[i]) - 48 + prev
+			if sam[i] == 43 || sam[i] == 45 {
+				prev = prev
+			} else {
+				prev = prev * 10
+				prev = int(sam[i]) - 48 + prev
+			}
 		} else {
 			return 0
 		}
 	}
-	if checkminus == 1 {
-		return -prev
+	if checkplus == 1 {
+		return prev
 	}
-	return prev
+	return -prev
 }
