edd1d78ae3be7c3418744f98cc0d7d83c9075f0b
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-01 06:27:17 +0600

    asd

--- basicatoi.go
+++ basicatoi.go
@@ -2,11 +2,16 @@ package piscine
 
 func BasicAtoi(s string) int {
 	sam := []rune(s)
-	summa = 0
+	summa := 0
+	z := 0
 	for i := 0; i < len(s); i++ {
 		if sam[i] > 48 && sam[i] < 58 {
-
+			for q := 0; q < z; q++ {
+				summa = summa * 10
+			}
+			z = z + 1
+			summa = (sam[i] - 48 + summa)
 		}
 	}
-	return string(reversenoyet)
+	return summa
 }
