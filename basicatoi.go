096cfd9e0ca85229a9f75c13f929cd054f3b89a9
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-01 06:59:26 +0600

    asd

--- basicatoi.go
+++ basicatoi.go
@@ -4,7 +4,7 @@ func BasicAtoi(s string) int {
 	sam := []rune(s)
 	prev := 0
 	for i := 0; i < len(s); i++ {
-		if sam[i] > 48 && sam[i] < 58 {
+		if sam[i] > 47 && sam[i] < 58 {
 			prev = prev * 10
 			prev = int(sam[i]) - 48 + prev
 		}
