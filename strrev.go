404406b2896b3a422f16227147aa29438a33442b
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-01 06:01:59 +0600

    asd

--- strrev.go
+++ strrev.go
@@ -3,7 +3,7 @@ package piscine
 func StrRev(s string) string {
 	reversenoyet := []byte(s)
 	for i := 0; i < len(s); i++ {
-		reversenoyet[i] = s[len(s)-i -1]
+		reversenoyet[i] = s[len(s)-i-1]
 	}
 	return string(reversenoyet)
 }
