93d43c74f9a5fb450c324c8b0a95c1f100394950
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-03 19:49:01 +0600

    45

---
+++ fibonacci.go
@@ -0,0 +1,16 @@
+package piscine
+
+func fibonacci(index int) int {
+
+	if index < 0 {
+		return -1
+	} else if index == 0 {
+		return 0
+	} else if index == 1 || index == 2 {
+		return 1
+	} else if index == 3 {
+		return 2
+	} else {
+		return fibonacci(index-1) + fibonacci(index-2)
+	}
+}
