fe49745d0a8f6755fa7ac4dd05d9a44b5276e578
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-04 05:17:26 +0600

    48

--- findnextprime.go
+++ findnextprime.go
@@ -4,28 +4,10 @@ func FindNextPrime(nb int) int {
 	if nb <= 2 {
 		return 2
 	}
-	check := 0
 	for i := 2; i < nb; i++ {
 		if nb%i == 0 {
-			check = 1
+			return FindNextPrime(nb + 1)
 		}
 	}
-	if check == 0 {
-		return nb
-	} else {
-		check = 0
-		for z := nb + 1; ; z++ {
-			for i := 2; i < z; i++ {
-				if z%i == 0 {
-					check = 1
-				}
-			}
-			if check == 0 {
-				return z
-			} else {
-				check = 0
-			}
-		}
-	}
-	return 0
+	return nb
 }
