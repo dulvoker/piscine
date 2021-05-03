c3a273e3629a214177bf5b0a29b95f8d286630ff
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-03 17:06:14 +0600

    q4t1

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,13 +1,16 @@
-package piscine
+package main
 
 func IterativeFactorial(nb int) int {
 	sum := 1
-	if nb > 1 {
+	if nb >= 1 {
 		for i := 1; i <= nb; i++ {
 			sum = sum * i
 		}
 		return sum
 	} else {
+		if nb == 0 {
+			return 1
+		}
 		return 0
 	}
 }
