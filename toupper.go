c6da62656d00ed4a609f49ea0b3f1bfefcfb91e2
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-04 18:05:20 +0600

    513

--- toupper.go
+++ toupper.go
@@ -3,7 +3,7 @@ package piscine
 func ToUpper(s string) string {
 	casted := []rune(s)
 	for i := 0; i < len(s); i++ {
-		if casted[i] > 97 && casted[i] < 122 {
+		if casted[i] > 96 && casted[i] < 123 {
 			casted[i] = rune(casted[i] - 32)
 		}
 	}
