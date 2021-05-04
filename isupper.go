fe622471e78b164cc33cd45eb74fd7a004b1dae8
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-04 17:26:17 +0600

    58

--- isupper.go
+++ isupper.go
@@ -1,7 +1,7 @@
 package piscine
 
 func IsUpper(s string) bool {
-	casted = []rune(IsUpper)
+	casted := []rune(IsUpper)
 	for i := 0; i < len(IsUpper); i++ {
 		if casted[i] < 65 || casted[i] > 90 {
 			return false
