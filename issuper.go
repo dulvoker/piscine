60178307bc30ef186674f4cced6a63b4c8a8f612
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-04 17:22:22 +0600

    58

---
+++ issuper.go
@@ -0,0 +1,11 @@
+package piscine
+
+func IsUpper(s string) bool {
+	casted = []rune(IsUpper)
+	for i := 0; i < len(IsUpper); i++ {
+		if casted[i] < 65 || casted[i] > 90 {
+			return false
+		}
+	}
+	return true
+}
