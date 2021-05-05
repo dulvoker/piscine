31ce623116dded62a849ee2edf8e4c267843b27e
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-06 04:51:19 +0600

    61

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	prog_name := os.Args[0]
 	casted := []rune(prog_name)
-	for i := 0; i < len(prog_name); i++ {
+	for i := 2; i < len(prog_name); i++ {
 		z01.PrintRune(casted[i])
 	}
 }
