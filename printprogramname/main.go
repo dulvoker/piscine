6cb9c423d33ae9ef12236d45e541a83a7b086bfe
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-06 05:10:59 +0600

    61

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -7,8 +7,7 @@ import (
 )
 
 func main() {
-	prog_name := os.Args[0]
-	prog_name = string(prog_name)
+	prog_name := string(os.Args[0])
 	for _, word := range prog_name[2:] {
 		z01.PrintRune(word)
 	}
