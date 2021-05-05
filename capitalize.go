1e8057bdfc979892a865d063337c40bce3e2d924
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-05 07:09:22 +0600

    519

---
+++ capitalize.go
@@ -0,0 +1,27 @@
+package piscine
+
+func Capitalize(s string) string {
+	casted := []rune(s)
+	check1 := 0
+	for i := 0; i < len(s); i++ {
+		if casted[i] > 64 && casted[i] < 91 || casted[i] > 96 && casted[i] < 123 {
+			if check1 == 0 {
+				if casted[i] > 96 {
+					casted[i] = casted[i] - 32
+				}
+				i++
+				check1 = 1
+			}
+			if check1 == 1 {
+				if casted[i] > 91 {
+					casted[i] = casted[i]
+				} else {
+					casted[i] = casted[i] + 32
+				}
+			}
+		} else {
+			check1 = 0
+		}
+	}
+	return string(casted)
+}
