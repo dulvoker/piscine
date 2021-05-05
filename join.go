3a7c0c8fc29f42e70a0636c275f639e7d89e3cf4
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-05 09:19:45 +0600

    525

--- join.go
+++ join.go
@@ -2,7 +2,10 @@ package piscine
 
 func Join(strs []string, sep string) string {
 	chetam := ""
-	for _, word := range strs {
+	for index, word := range strs {
+		if index == len(strs)-1 {
+			chetam = chetam + word
+		}
 		chetam = chetam + word + sep
 	}
 	return chetam
