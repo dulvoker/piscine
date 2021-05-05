5f9b023f3d3d223eb869f005a42c0dfa6d52e9ff
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-05 09:04:39 +0600

    521

--- capitalize.go
+++ capitalize.go
@@ -4,20 +4,18 @@ func Capitalize(s string) string {
 	casted := []rune(s)
 	check1 := 0
 	for i := 0; i < len(s); i++ {
-		if casted[i] > 64 && casted[i] < 91 || casted[i] > 96 && casted[i] < 123 {
+		if casted[i] > 64 && casted[i] < 91 || casted[i] > 96 && casted[i] < 123 || casted[i] > 47 && casted[i] < 58 {
 			if check1 == 0 {
 				if casted[i] > 96 {
 					casted[i] = casted[i] - 32
 				}
 				check1 = 1
-				continue
-			}
-			if check1 == 1 {
-				if casted[i] < 91 {
+			} else if check1 == 1 {
+				if casted[i] < 91 && casted[i] > 64 {
 					casted[i] = casted[i] + 32
 				}
 			}
-		} else if casted[i] < 47 || casted[i] > 57 {
+		} else {
 			check1 = 0
 		}
 	}
