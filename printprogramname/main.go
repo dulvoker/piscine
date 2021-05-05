ca728423988fd4a001eefac2f5b87286138a6935
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-06 04:40:36 +0600

    61

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,14 +1,15 @@
 package piscine
 
 import (
-	"github.com/01-edu/z01"
 	"os"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
 	prog_name := os.Args[0]
 	casted := []rune(prog_name)
 	for i := 0; i < len(prog_name); i++ {
-		z01.PrintRune(rune(casted[i]))
+		z01.PrintRune(casted[i])
 	}
 }
