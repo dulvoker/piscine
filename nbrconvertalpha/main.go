dd1eab13ec134c3dfbca9e7aa570be97b17fa261
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-06 17:38:33 +0600

    64

---
+++ nbrconvertalpha/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func calc(s string) rune {
+	sam := []rune(s)
+	prev := 0
+	for i := 0; i < len(s); i++ {
+		if sam[i] > 47 && sam[i] < 58 {
+			prev = prev * 10
+			prev = int(sam[i]) - 48 + prev
+		}
+	}
+	if prev < 0 || prev > 25 {
+		return rune(32)
+	}
+	return rune(prev + 48 + 48)
+}
+
+func upper(s string) rune {
+	sam := []rune(s)
+	prev := 0
+	for i := 0; i < len(s); i++ {
+		if sam[i] > 47 && sam[i] < 58 {
+			prev = prev * 10
+			prev = int(sam[i]) - 48 + prev
+		}
+	}
+	if prev < 0 || prev > 25 {
+		return rune(32)
+	}
+	return rune(prev + 48 + 16)
+}
+
+func main() {
+	if os.Args[1] == "--upper" {
+		for _, word := range os.Args[2:] {
+			z01.PrintRune(upper(word))
+		}
+	} else {
+		for _, word := range os.Args[1:] {
+			z01.PrintRune(calc(word))
+		}
+	}
+}
