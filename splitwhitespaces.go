2e8eb51d8b13b0552aa47ead8e8d4040e382a5b1
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-07 01:02:50 +0600

    75

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -4,7 +4,7 @@ func SplitWhiteSpaces(s string) []string {
 	word := ""
 	var words []string
 	for i, char := range s {
-		if i == len(s)-1 {
+		if i == len(s)-1 && char != rune(32) {
 			words = append(words, word+string(char))
 		} else if char != rune(32) && char != rune(9) && char != rune(10) {
 			word = word + string(char)
