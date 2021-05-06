4b46a813179411c61b0518090ca56a05a5c7bd21
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-06 17:50:16 +0600

    65

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -30,14 +30,15 @@ func upper(s string) rune {
 			prev = int(sam[i]) - 48 + prev
 		}
 	}
-	if prev < 0 || prev > 25 {
+	if prev <= 0 || prev > 25 {
 		return rune(32)
 	}
 	return rune(prev + 48 + 16)
 }
 
 func main() {
-	if os.Args[1] == "--upper" {
+	if len(os.Args) == 1 {
+	} else if os.Args[1] == "--upper" {
 		for _, word := range os.Args[2:] {
 			z01.PrintRune(upper(word))
 		}
