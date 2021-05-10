c0726b09fb1f4e2b8a4c0bb1ee6436caf7859db0
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-10 15:56:08 +0600

    83

--- point/main.go
+++ point/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"github.com/01-edu/z01"
 )
 
 type point struct {
@@ -14,10 +14,31 @@ func setPoint(ptr *point) {
 	ptr.y = 21
 }
 
+func PrintNbr(n int) string {
+	var table []rune
+	for i := 0; ; i++ {
+		table = append(table, rune(n%10+48))
+		if n/10 == 0 {
+			break
+		}
+		n = n / 10
+	}
+	newstring := ""
+	lenq := len(table)
+	for i := 0; i < lenq; i++ {
+		newstring = newstring + string(table[lenq-i-1])
+	}
+	return newstring
+}
+
 func main() {
 	points := point{}
 
 	setPoint(&points)
 
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+	for_print := "x = " + PrintNbr(points.x) + ", y = " + PrintNbr(points.y) + "\n"
+
+	for _, char := range for_print {
+		z01.PrintRune(char)
+	}
 }
