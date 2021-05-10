57028a1e6b1d4cdbd8db9fdedd6db00df430016f
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-10 16:20:53 +0600

    83

--- displayfile/main.go
+++ displayfile/main.go
@@ -11,12 +11,13 @@ func main() {
 	} else if len(os.Args) > 2 {
 		fmt.Println("Too many arguments")
 	} else {
-		file, err := os.Open(string(os.Args[1]))
+		file, err := os.Open(os.Args[1])
+		fmt.Println(os.Args[1])
 		if err != nil {
 			fmt.Println("File name missing")
 		}
-		var table []byte
-		file.Read(table)
-		fmt.Println(string(table))
+		arr := make([]byte, 100)
+		file.Read(arr)
+		fmt.Println(string(arr))
 	}
 }
