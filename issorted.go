9c9549a3606cda7a7eb28ef66937cf626c471a6d
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-11 19:45:30 +0600

    84

---
+++ issorted.go
@@ -0,0 +1,21 @@
+package piscine
+
+func f(a, b int) int {
+	if b > a {
+		return -1
+	} else if a == b {
+		return 0
+	}
+	return 1
+}
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	for i, each := range a {
+		if i == len(a)-1 {
+			break
+		} else if f(each, a[i+1]) > 0 {
+			return false
+		}
+	}
+	return true
+}
