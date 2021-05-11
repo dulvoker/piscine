e4ca80dacf374f2997dcab9c3ca3e7d4680ceffd
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-11 20:17:37 +0600

    84

--- issorted.go
+++ issorted.go
@@ -6,7 +6,7 @@ func f(a, b int) int {
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	for i, each := range a {
-		if i == len(a)-2 {
+		if i == len(a)-2 || len(a) > 3 {
 			break
 		}
 		if f(each, a[i+1])*f(a[i+1], a[i+2]) < 0 {
