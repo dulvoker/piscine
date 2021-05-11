63efe97ec978ab1d53951213fb39ad1830c0b7d0
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-11 18:55:25 +0600

    81

--- foreach.go
+++ foreach.go
@@ -1,13 +1,7 @@
 package piscine
 
-import "github.com/01-edu/z01"
-
-func PrintNbr(b int) {
-	z01.PrintRune(rune(b + 48))
-}
-
 func ForEach(f func(int), numbers []int) {
 	for _, each := range numbers {
-		PrintNbr(each)
+		f(each)
 	}
 }
