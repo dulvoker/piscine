04b481cd71231e60714f9c46acc8522f8009ecd5
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-11 18:49:32 +0600

    81

---
+++ foreach.go
@@ -0,0 +1,12 @@
+import "github.com/01-edu/z01"
+
+func PrintNbr(b int) {
+	z01.PrintRune(rune(b + 48))
+}
+
+func ForEach(f func(int), numbers []int) {
+	for _, each := range numbers {
+		PrintNbr(each)
+	}
+}
+
