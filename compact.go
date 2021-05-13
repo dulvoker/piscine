925c2652089e0561d9d4eb0913ca4764c04ddb21
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-13 20:24:48 +0600

    sad

--- compact.go
+++ compact.go
@@ -9,14 +9,6 @@ func Compact(ptr *[]string) int {
 			table = append(table, char)
 		}
 	}
-	for i := 0; i < len(*ptr); i++ {
-		if i < sum {
-			(*ptr)[i] = table[i]
-		} else {
-			(*ptr)[i] = string(rune(0))
-		}
-	}
-
-	(*ptr) = (*ptr)[:sum]
+	(*ptr) = table
 	return sum
 }
