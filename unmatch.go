1c424b42248acb39cdf570b26733805ceeceaa4d
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-14 04:36:10 +0600

    asd

--- unmatch.go
+++ unmatch.go
@@ -6,10 +6,10 @@ func Unmatch(a []int) int {
 		check = 0
 		for q := 0; q < len(a); q++ {
 			if a[i] == a[q] && q != i {
-				check = 1
+				check++
 			}
 		}
-		if check == 0 {
+		if check%2 != 0 {
 			return a[i]
 		}
 	}
