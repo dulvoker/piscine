a4ba0ce8e9edebcc259aa8e0e543d506f9908a83
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-13 19:24:01 +0600

    asd

---
+++ fixthemain/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "github.com/01-edu/z01"
+
+type Door struct {
+	state bool
+}
+
+func PrintStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+}
+
+func CloseDoor(ptrDoor *Door) {
+	PrintStr("Door Closing...")
+	ptrDoor.state = false
+}
+func OpenDoor(ptrDoor *Door) {
+	PrintStr("Door Opening...")
+	ptrDoor.state = true
+}
+
+func IsDoorOpen(ptrDoor *Door) bool {
+	PrintStr("is the Door opened ?")
+	return ptrDoor.state
+}
+
+func IsDoorClose(ptrDoor *Door) bool {
+	PrintStr("is the Door closed ?")
+	if ptrDoor.state == true {
+		return false
+	}
+	return true
+}
+
+func main() {
+	door := Door{}
+
+	OpenDoor(&door)
+	if IsDoorClose(&door) {
+		OpenDoor(&door)
+	}
+	if IsDoorOpen(&door) {
+		CloseDoor(&door)
+	}
+	if door.state == true {
+		CloseDoor(&door)
+	}
+}
