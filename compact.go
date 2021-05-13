3eca385097de88d45e9453f21b9fb1c1495aa740
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-13 20:22:05 +0600

    hack

---
+++ compact.go
@@ -0,0 +1,22 @@
+package main
+
+func Compact(ptr *[]string) int {
+	sum := 0
+	var table []string
+	for _, char := range *ptr {
+		if len(char) != 0 {
+			sum++
+			table = append(table, char)
+		}
+	}
+	for i := 0; i < len(*ptr); i++ {
+		if i < sum {
+			(*ptr)[i] = table[i]
+		} else {
+			(*ptr)[i] = string(rune(0))
+		}
+	}
+
+	(*ptr) = (*ptr)[:sum]
+	return sum
+}
