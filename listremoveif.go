d313139194bcaeb8c1a33d28356bc8b5be1d0640
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-18 17:43:11 +0600

    asd

--- listremoveif.go
+++ listremoveif.go
@@ -5,7 +5,11 @@ func ListRemoveIf(l *List, data_ref interface{}) {
 	lis = l.Head
 	for lis != nil {
 		if CompStr(lis.Next.Data, data_ref) {
-			lis.Next = lis.Next.Next
+			if lis.Next.Next != nil {
+				lis.Next = lis.Next.Next
+			} else {
+				lis.Next = nil
+			}
 		}
 		lis = lis.Next
 	}
