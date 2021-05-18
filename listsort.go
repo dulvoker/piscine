01b783c6705da8b84cc9c0276500295fc2e4bd99
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-19 01:04:26 +0600

    asd

--- listsort.go
+++ listsort.go
@@ -1,5 +1,30 @@
 package piscine
 
+func SortTable(table []int) {
+	len := len(table)
+	for i := 0; i < len-1; i++ {
+		for j := 0; j < len-i-1; j++ {
+			if table[j] < table[j+1] {
+				table[j], table[j+1] = table[j+1], table[j]
+			}
+		}
+	}
+}
+
+func listPushBack(l *NodeI, data int) *NodeI {
+	n := &NodeI{Data: data}
+
+	if l == nil {
+		return n
+	}
+	iterator := l
+	for iterator.Next != nil {
+		iterator = iterator.Next
+	}
+	iterator.Next = n
+	return l
+}
+
 type NodeI struct {
 	Data int
 	Next *NodeI
@@ -13,13 +38,10 @@ func ListSort(l *NodeI) *NodeI {
 		table = append(table, lis.Data)
 		lis = lis.Next
 	}
-	SortIntegerTable(table)
-	lis.Data = table[0]
-	var node *NodeI
-	for _, each := range table[1:] {
-		node = &NodeI{Data: each}
-		l.Next = node
-		l = l.Next
+	SortTable(table)
+	lis = nil
+	for _, each := range table {
+		lis = listPushBack(lis, each)
 	}
 	return l
 }
