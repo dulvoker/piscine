86c2dcd57468be33b26c9be4b532832e46d24237
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-18 15:53:46 +0600

    asd

--- listforeach.go
+++ listforeach.go
@@ -8,3 +8,21 @@ func ListForEach(l *List, f func(*NodeL)) {
 		lis = lis.Next
 	}
 }
+
+func Add2_node(node *NodeL) {
+	switch node.Data.(type) {
+	case int:
+		node.Data = node.Data.(int) + 2
+	case string:
+		node.Data = node.Data.(string) + "2"
+	}
+}
+
+func Subtract3_node(node *NodeL) {
+	switch node.Data.(type) {
+	case int:
+		node.Data = node.Data.(int) - 3
+	case string:
+		node.Data = node.Data.(string) + "-3"
+	}
+}
