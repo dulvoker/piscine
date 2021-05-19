46b58d2114c3e7f6ad757db7a6f9aab74833cece
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-19 15:15:21 +0600

    asd

--- sortlistinsert.go
+++ sortlistinsert.go
@@ -1,8 +1,18 @@
 package piscine
 
 func SortListInsert(l *NodeI, data_ref int) *NodeI {
-	ListSort(l)
-	l = listPushBack(l, data_ref)
-	ListSort(l)
-	return l
+	var table []int
+	var lis *NodeI
+	lis = l
+	for lis != nil {
+		table = append(table, lis.Data)
+		lis = lis.Next
+	}
+	table = append(table, data_ref)
+	SortTable(table)
+	lis = nil
+	for _, each := range table {
+		lis = listPushBack(lis, each)
+	}
+	return lis
 }
