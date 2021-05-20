6959be46a3e12d0f9368cba92a87c4989a65a326
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-21 04:17:56 +0600

    asd

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -4,10 +4,8 @@ func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
 	if root.Data == elem {
 		return root
 	}
-	if root.Data > elem {
-		BTreeSearchItem(root.Left, elem)
-	} else {
-		BTreeSearchItem(root.Right, elem)
+	if root.Data < elem {
+		return BTreeSearchItem(root.Right, elem)
 	}
-	return root
+	return BTreeSearchItem(root.Left, elem)
 }
