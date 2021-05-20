8d0d6666e50e53dde61d01423b67a88edacb4c05
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-21 04:51:26 +0600

    asd

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -2,19 +2,19 @@ package piscine
 
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
 	if root == nil {
-		return root.Parent
+		return nil
 	}
 	if root.Data == elem {
 		return root
 	}
 	if root.Data < elem {
 		if root.Right == nil {
-			return root
+			return nil
 		}
 		return BTreeSearchItem(root.Right, elem)
 	}
 	if root.Left == nil {
-		return root
+		return nil
 	}
 	return BTreeSearchItem(root.Left, elem)
 }
