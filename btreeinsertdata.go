a9e97b666cb183f759aed2fe36857f8f3392388e
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-20 20:07:43 +0600

    sa

---
+++ btreeinsertdata.go
@@ -0,0 +1,18 @@
+package piscine
+
+type TreeNode struct {
+	Left, Right, Parent *TreeNode
+	Data                string
+}
+
+func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+	node := &TreeNode{Data: data}
+	if root == nil {
+		root = node
+	} else if root.Data > node.Data {
+		root.Left = BTreeInsertData(root.Left, data)
+	} else {
+		root.Right = BTreeInsertData(root.Right, data)
+	}
+	return root
+}
