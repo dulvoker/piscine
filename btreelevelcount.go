79e08819310fb23514249146b38ed216da5881e6
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-21 05:07:30 +0600

    asd

--- btreelevelcount.go
+++ btreelevelcount.go
@@ -2,7 +2,12 @@ package piscine
 
 func BTreeLevelCount(root *TreeNode) int {
 	if root != nil {
-		return (BTreeLevelCount(root.Left) + BTreeLevelCount(root.Right) + 1)
+		rightside := BTreeLevelCount(root.Right) + 1
+		leftside := BTreeLevelCount(root.Left) + 1
+		if rightside > leftside {
+			return rightside
+		}
+		return leftside
 	} else {
 		return 0
 	}
