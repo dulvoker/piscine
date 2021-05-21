9f425832415151acea1cf0cda5d9032944c31642
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-22 03:28:13 +0600

    asd

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -1,10 +1,29 @@
 package piscine
 
-func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
+func fapply(root *TreeNode, level int, f func(...interface{}) (int, error)) {
 	if root == nil {
 		return
 	}
-	f(root.Data)
-	BTreeApplyByLevel(root.Left, f)
-	BTreeApplyByLevel(root.Left.Parent, f)
+	if level == 1 {
+		f(root.Data)
+	} else if level > 1 {
+		fapply(root.Left, level-1, f)
+		fapply(root.Right, level-1, f)
+	}
+}
+func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
+	if root != nil {
+		a := BTreeLevelCount(root)
+		for b := 1; b <= a; b++ {
+			if root == nil {
+				return
+			}
+			if b == 1 {
+				f(root.Data)
+			} else if b > 1 {
+				fapply(root.Left, b-1, f)
+				fapply(root.Right, b-1, f)
+			}
+		}
+	}
 }
